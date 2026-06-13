cb882ad1e419a730e4f38c84dcf0ef5e1aabad96
malek <malekmhggg@gmail.com>
2026-06-13 14:34:13 +0300

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Your Flask website is running!"
+    return "<h1>Hello! This is a Python website.</h1>"
 
 if __name__ == "__main__":
     app.run()
