8a1cfd699dabdd65fed2c4d1e371b00eb7910abe
malek <malekmhggg@gmail.com>
2026-06-13 15:20:44 +0300

    fixed

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/lessons")
+def lessons():
+    return "<h1>Lessons Page</h1>"
+
+@app.route("/practice")
+def practice():
+    return "<h1>Practice Page</h1>"
+
+@app.route("/categories")
+def categories():
+    return "<h1>Categories Page</h1>"
+
 
 if __name__ == "__main__":
     app.run()
